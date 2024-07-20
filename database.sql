81c26ac380a3d1019fcb47d9f4a4e083f90af1cb
Priyanshu Prasad Gupta <priyanshoon.pg@gmail.com>
2024-07-20 11:16:22 +0530

    feat: GET todo lists

--- database.sql
+++ database.sql
@@ -1,1 +1,15 @@
+-- Create the users table
+CREATE TABLE users (
+  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  password VARCHAR(255) NOT NULL
+);
 
+-- Create the todo_list table with a foreign key referencing users.id
+CREATE TABLE todo_lists (
+  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  user_id UUID REFERENCES users(id) ON DELETE CASCADE,
+  task VARCHAR(255) NOT NULL,
+  completed BOOLEAN DEFAULT FALSE
+);
--- server/routes/Todo.routes.js
+++ server/routes/Todo.routes.js
@@ -9,7 +9,19 @@ router.get("/users/:user_id", async (req, res) => {
     let user_id = req.params.user_id
     if (User.findByPk(user_id) != null) {
         let todoData = await TodoList.findAll({ where: { user_id: user_id } })
+        let todo_item = []
+        for (let i = 0; i < todoData.length; i++) {
+            todo_item.push(todoData[i].dataValues.task)
+        }
+        res.status(200).json(todo_item)
+    } else {
+        res.status(404).send("User not found")
     }
+})
+
+//  NOTE: Post Todo list
+router.post("/users/:user_id", async (req, res) => {
+
 })
 
 module.exports = router
